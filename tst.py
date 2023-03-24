1368100694a04ba39283e42cab4c00426970bee6
RainRahni <rain.rahni@mail.ee>
2023-03-24 21:15:01 +0200

    decrementing counter rather than increasing it

--- tst.py
+++ tst.py
@@ -12,31 +12,23 @@ def get_payday(year, month):
     # if 10th workday is either weekend or national holiday, then payday is last workday.
     while 7 > pay_date.weekday() > 4 or pay_date in holidays.Estonia(year):
         pay_date -= datetime.timedelta(days=1)
-    """pay_date_weekday_as_int = pay_date.weekday()
-    if pay_date_weekday_as_int == 5 or (pay_date in holidays.Estonia(year) and 6 > pay_date_weekday_as_int > 0):
-        pay_date -= datetime.timedelta(days=1)
-    elif pay_date_weekday_as_int == 6:
-        pay_date -= datetime.timedelta(days=2)
-    elif pay_date_weekday_as_int == 0 and pay_date in holidays.Estonia(year):
-        pay_date -= datetime.timedelta(days=3)"""
-    # Find the reminder date - 3 workdays before pay date.
-    reminder_date = pay_date - datetime.timedelta(days=1)
-    counter = 3
-    #7 > reminder_date.weekday() > 4 or pay_date in holidays.Estonia(year)
 
+    # Find the reminder date = 3 workdays before pay date.
+    reminder_date = pay_date - datetime.timedelta(days=1)
+    # Counter for work days.
+    counter = 3
     while counter != 0:
+        # Subtract 1 day from remainder date.
         reminder_date -= datetime.timedelta(days=1)
+        # if it is weekday and not holiday, decrement counter by 1
         if reminder_date.weekday() < 5 and reminder_date not in holidays.Estonia(year):
             counter -= 1
-
-    """if 3 >= pay_date.weekday() >= 0:
-        reminder_date = pay_date - datetime.timedelta(days=6)
-    elif (pay_date.weekday() - 4) == 0 and pay_date in holidays.Estonia(year):
-        reminder_date = pay_date - datetime.timedelta(days=7)
-    elif (pay_date.weekday() - 4) == 1 and pay_date in holidays.Estonia(year):
-        reminder_date = pay_date - datetime.timedelta(days=5)
-    else:
-        reminder_date = pay_date - datetime.timedelta(days=4)"""
+        # Next day date
+        next_day_date = reminder_date - datetime.timedelta(days=1)
+        # Check if counter is 0, pay date is Monday and so that next day will not be either saturday, sunday or holiday.
+        if counter == 0 and pay_date.weekday() == 0 and next_day_date.weekday() < 5 \
+                and next_day_date not in holidays.Estonia(year):
+            reminder_date -= datetime.timedelta(days=1)
     return pay_date, reminder_date
 
 
@@ -49,11 +41,12 @@ def write_csv_file(year):
         # Get pay date and reminder date for each month and write it to file.
         for month in range(1, 13):
             pay_date, reminder_date = get_payday(year, month)
-            writer.writerow([datetime.date(year, month, 1).strftime("%B"), pay_date.strftime("%d.%m.%Y"), reminder_date.strftime("%d.%m.%Y")])
+            writer.writerow([datetime.date(year, month, 1).strftime("%B"), pay_date.strftime("%d.%m.%Y"),
+                             reminder_date.strftime("%d.%m.%Y")])
 
 
 def main():
-    # Looge argumentide parser, et lugeda sisse aastaarv
+    # Create argument parser.
     parser = argparse.ArgumentParser(description="Generate payday dates for Spintek.")
     parser.add_argument("year", type=int, help="Aastaarv")
 
@@ -64,5 +57,4 @@ def main():
 
 
 if __name__ == "__main__":
-    print(get_payday(2023, 7)) #4
     main()
