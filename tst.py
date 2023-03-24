4d4d0b00152f447d121f5198e7ff38dd3d06b3dd
RainRahni <rain.rahni@mail.ee>
2023-03-24 17:33:35 +0200

    changed so that reminder date has its own weekday int

--- tst.py
+++ tst.py
@@ -3,27 +3,29 @@ import datetime
 import holidays
 import argparse
 
+
 def get_payday(year, month):
     """Leiab palgamaksmise kuupäeva ja meeldetuletuse kuupäeva antud aasta ja kuu kohta."""
     # Find 10nth day of given year and month.
     pay_date = datetime.date(year, month, 10)
 
     # if 10th workday is either weekend or national holiday, then payday is last workday.
-    weekday_as_int = pay_date.weekday()
-    if weekday_as_int == 5 or (pay_date in holidays.Estonia(year) and 6 > weekday_as_int > 0):
+    pay_date_weekday_as_int = pay_date.weekday()
+    if pay_date_weekday_as_int == 5 or (pay_date in holidays.Estonia(year) and 6 > pay_date_weekday_as_int > 0):
         pay_date -= datetime.timedelta(days=1)
-    elif weekday_as_int == 6:
+    elif pay_date_weekday_as_int == 6:
         pay_date -= datetime.timedelta(days=2)
-    elif weekday_as_int == 0 and pay_date in holidays.Estonia(year):
+    elif pay_date_weekday_as_int == 0 and pay_date in holidays.Estonia(year):
         pay_date -= datetime.timedelta(days=3)
 
     # Find the reminder date - 3 workdays before pay date.
     reminder_date = pay_date - datetime.timedelta(days=3)
-    if weekday_as_int == 5 or (reminder_date in holidays.Estonia(year) and 6 > weekday_as_int > 0) :
+    reminder_date_weekday_as_int = reminder_date.weekday()
+    if reminder_date_weekday_as_int == 5 or (reminder_date in holidays.Estonia(year) and 6 >reminder_date_weekday_as_int > 0) :
         reminder_date -= datetime.timedelta(days=1)
-    elif weekday_as_int == 6:
+    elif reminder_date_weekday_as_int == 6:
         pay_date -= datetime.timedelta(days=2)
-    elif weekday_as_int == 0 and reminder_date in holidays.Estonia(year):
+    elif reminder_date_weekday_as_int == 0 and reminder_date in holidays.Estonia(year):
         pay_date -= datetime.timedelta(days=3)
     return pay_date, reminder_date
 
