2b15507b1f49ea11f1fc5b85690098cab48716b0
RainRahni <rain.rahni@mail.ee>
2023-03-24 19:36:25 +0200

    commented ifs and added while loops.

--- tst.py
+++ tst.py
@@ -10,23 +10,29 @@ def get_payday(year, month):
     pay_date = datetime.date(year, month, 10)
 
     # if 10th workday is either weekend or national holiday, then payday is last workday.
-    pay_date_weekday_as_int = pay_date.weekday()
+    while 7 > pay_date.weekday() > 4 or pay_date in holidays.Estonia(year):
+        pay_date -= datetime.timedelta(days=1)
+    """pay_date_weekday_as_int = pay_date.weekday()
     if pay_date_weekday_as_int == 5 or (pay_date in holidays.Estonia(year) and 6 > pay_date_weekday_as_int > 0):
         pay_date -= datetime.timedelta(days=1)
     elif pay_date_weekday_as_int == 6:
         pay_date -= datetime.timedelta(days=2)
     elif pay_date_weekday_as_int == 0 and pay_date in holidays.Estonia(year):
-        pay_date -= datetime.timedelta(days=3)
-
+        pay_date -= datetime.timedelta(days=3)"""
     # Find the reminder date - 3 workdays before pay date.
-    if 3 >= pay_date.weekday() >= 0:
+    reminder_date = pay_date - datetime.timedelta(days=4)
+    #counter = 4
+    while 7 > reminder_date.weekday() > 4 or pay_date in holidays.Estonia(year):
+        reminder_date -= datetime.timedelta(days=1)
+        #counter += 1
+    """if 3 >= pay_date.weekday() >= 0:
         reminder_date = pay_date - datetime.timedelta(days=6)
     elif (pay_date.weekday() - 4) == 0 and pay_date in holidays.Estonia(year):
         reminder_date = pay_date - datetime.timedelta(days=7)
     elif (pay_date.weekday() - 4) == 1 and pay_date in holidays.Estonia(year):
         reminder_date = pay_date - datetime.timedelta(days=5)
     else:
-        reminder_date = pay_date - datetime.timedelta(days=4)
+        reminder_date = pay_date - datetime.timedelta(days=4)"""
     return pay_date, reminder_date
 
 
